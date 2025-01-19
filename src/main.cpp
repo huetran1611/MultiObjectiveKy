75e1d112307356de0ff6ba45e9ad6ee0dfdd82e9
KyHoang0810 <ky.ht210505@sis.hust.edu.vn>
2025-01-19 22:58:04 +0700

    tabu limit time 4 last #2

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,6 @@ int main(int argc,char *argv[]){
         pareto=tabu_search2(init,max_tabu_iter);
         //pareto= NSGA2(populationcopy);;
         /*
-
         /int paretosize=pareto.size();
         double minfit1=pareto[0].fitness1;
         double minfit2=pareto[paretosize-1].fitness2;
