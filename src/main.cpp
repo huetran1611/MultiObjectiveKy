fbc97bb1345739b2c0d8e94301cd3ff1dc68ff04
KyHoang0810 <ky.ht210505@sis.hust.edu.vn>
2025-01-19 22:57:36 +0700

    tabu limit time 4 last #1

--- src/main.cpp
+++ src/main.cpp
@@ -97,11 +97,11 @@ int main(int argc,char *argv[]){
         populationcopy=population;
         vector<Individual> pareto;
         pareto.clear();
-        //vector<int> init=generateSol3();
-        //pareto=tabu_search2(init,max_tabu_iter);
-        pareto= NSGA2(populationcopy);;
+        vector<int> init=generateSol3();
+        pareto=tabu_search2(init,max_tabu_iter);
+        //pareto= NSGA2(populationcopy);;
         /*
-        
+
         /int paretosize=pareto.size();
         double minfit1=pareto[0].fitness1;
         double minfit2=pareto[paretosize-1].fitness2;
